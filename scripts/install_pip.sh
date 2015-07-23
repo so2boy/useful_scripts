89ccfff2013a3cb754ba114bb847c5eec39cfd91
so2boy <yumeng11345@gmail.com>
2015-07-23 11:24:24 +0800

    Update and rename install_pexpect.sh to install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -8,12 +8,20 @@ wget https://bootstrap.pypa.io/ez_setup.py -O - --no-check-certificate | python 
 
 wget https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz --no-check-certificate
 
-tar zxf pexpect-3.3.tar.gz
+wget https://pypi.python.org/packages/source/p/pip/pip-7.1.0.tar.gz#md5=d935ee9146074b1d3f26c5f0acfd120e --no-check-certificate
 
-cd pexpect-3.3
+tar zxf pip-7.1.0.tar.gz
+
+cd pip-7.1.0
 
 python setup.py install --user
 
 cd /
 
 rm -rf $TMP_DIR
+
+echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
+
+# source ~/.bashrc
+# pip install --user pexpect
+# pip install --user request
