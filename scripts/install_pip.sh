3611668883becbf04c90261a57a701f70a816613
so2boy <yumeng11345@gmail.com>
2015-10-14 16:00:32 +0800

    Update install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-TMP_DIR=`mktemp -d /tmp/install_pexpect.XXXXX`
+OLD_DIR=`pwd`
+
+TMP_DIR=`mktemp -d /tmp/install_pip.XXXXX`
 
 cd $TMP_DIR
 
 wget https://bootstrap.pypa.io/ez_setup.py -O - --no-check-certificate | python - --user
-
-wget https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz --no-check-certificate
 
 wget https://pypi.python.org/packages/source/p/pip/pip-7.1.0.tar.gz#md5=d935ee9146074b1d3f26c5f0acfd120e --no-check-certificate
 
@@ -22,6 +22,10 @@ rm -rf $TMP_DIR
 
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
-# source ~/.bashrc
-# pip install --user pexpect
-# pip install --user request
+source ~/.bashrc
+
+pip install --user pexpect
+
+pip install --user requests
+
+cd $OLD_DIR
